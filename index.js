0a74036678981d8b9d1425258dc21548eae837a9
Kuldeep Parmar <kuldeepparmar246@gmail.com>
2024-08-23 23:59:38 +0530

    Add server using socket.io

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors({origin: 'http://localhost:3000'}));
+const server = createServer(app);
+const io = new Server(server,{cors: 'http://localhost:3000'});
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+});
+
+server.listen(5000, () => {
+  console.log('server running at http://localhost:5000');
+});
